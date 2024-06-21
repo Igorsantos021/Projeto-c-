22c9fccfce88ea7fc48b80bd3de63851be529849
Igor Santos <santosigor868@gmail.com>
2024-06-20 21:32:33 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Projeto_c_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.Label();
+            this.evento1 = new System.Windows.Forms.Label();
+            this.evento2 = new System.Windows.Forms.Label();
+            this.evento3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(280, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sorteio de Eventos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(200, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite seu nome:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(329, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(199, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Digite seu Email:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(329, 181);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(355, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 55);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sortear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(203, 360);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(35, 13);
+            this.resultado.TabIndex = 6;
+            this.resultado.Text = "label4";
+            // 
+            // evento1
+            // 
+            this.evento1.AutoSize = true;
+            this.evento1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evento1.Location = new System.Drawing.Point(566, 67);
+            this.evento1.Name = "evento1";
+            this.evento1.Size = new System.Drawing.Size(61, 13);
+            this.evento1.TabIndex = 7;
+            this.evento1.Text = "Evento01";
+            // 
+            // evento2
+            // 
+            this.evento2.AutoSize = true;
+            this.evento2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evento2.Location = new System.Drawing.Point(566, 108);
+            this.evento2.Name = "evento2";
+            this.evento2.Size = new System.Drawing.Size(61, 13);
+            this.evento2.TabIndex = 8;
+            this.evento2.Text = "Evento02";
+            // 
+            // evento3
+            // 
+            this.evento3.AutoSize = true;
+            this.evento3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evento3.Location = new System.Drawing.Point(566, 153);
+            this.evento3.Name = "evento3";
+            this.evento3.Size = new System.Drawing.Size(61, 13);
+            this.evento3.TabIndex = 9;
+            this.evento3.Text = "Evento03";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.evento3);
+            this.Controls.Add(this.evento2);
+            this.Controls.Add(this.evento1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Label evento1;
+        private System.Windows.Forms.Label evento2;
+        private System.Windows.Forms.Label evento3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_c_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random dado = new Random();
+            int valor;
+
+            valor = dado.Next(1, 4);
+
+            if (valor == 1)
+            {
+                evento1.Visible = true;
+                evento2.Visible = false;
+                evento3.Visible = false;
+            }
+
+            if(valor == 2)
+            {
+                evento1.Visible = false;
+                evento2.Visible = true;
+                evento3.Visible = false;
+            }
+
+            if(valor == 3)
+            {
+                evento1.Visible = false;
+                evento2.Visible = false;
+                evento3.Visible = true;
+            }
+
+            resultado.Text = Convert.ToString(valor);
+        }
+    }
+}
